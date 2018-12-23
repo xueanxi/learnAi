7a38af8268aafd58c75d9fe224ddd054fc3b6ef2
anxi.xue <xueanxi@163.com>
2018-12-23 15:10:23 +0000

    4

--- b/np3_linalg.py
+++ b/np3_linalg.py
@@ -13,5 +13,6 @@ matrix3 = np.mat([[1, 2, 3, 4], [6, 7, 8, 9], [11, 12, 13, 19], [16, 17, 18, 19]
 print('1: 矩阵的行列式')
 print(np.linalg.det(matrix1))
 
+#如果将矩阵和它的逆矩阵相乘，结果就应该是单位矩阵。pass:不是所有矩阵都有逆
 print('2: 矩阵的逆')
-print(np.linalg.inv(matrix3))
+print(np.linalg.inv(matrix1))
